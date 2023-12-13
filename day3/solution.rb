e96e852fc27a10d84ca4a777dd848d5f674b1aca
Jose Ferreira <josepferreira@netcabo.pt>
2023-12-13 20:24:40 +0000

    Day 3 and 4 solved

--- day3/solution.rb
+++ day3/solution.rb
@@ -4,12 +4,13 @@ def solve_part_one(matrix)
     has_symbol = false
     sum_of_number = 0
     number = ''
-    
+    number_start_index = nil
+
     while row_index <= matrix.length - 1
         while column_index <= matrix[row_index].length - 1
             if is_number(matrix[row_index][column_index])
                 number += matrix[row_index][column_index]
-                matrix[row_index][column_index] = 'x'
+                number_start_index = column_index if number_start_index.nil?
                 if !has_symbol
                     has_symbol = safe_is_symbol(matrix, row_index, column_index-1) || # left
                                     safe_is_symbol(matrix, row_index, column_index+1) || # right
@@ -25,8 +26,12 @@ def solve_part_one(matrix)
             else
                 if has_symbol
                     sum_of_number += Integer(number)
+                    (number_start_index..column_index - 1).each do |idx|
+                        matrix[row_index][idx] = 'X'
+                    end
                 end
 
+                number_start_index = nil
                 number = ''
                 has_symbol = false
             end
@@ -36,19 +41,82 @@ def solve_part_one(matrix)
 
         if has_symbol
             sum_of_number += Integer(number)
+            (number_start_index..column_index - 1).each do |idx|
+                matrix[row_index][idx] = 'X'
+            end
         end
 
+        number = ''
+        number_start_index = nil
+        has_symbol = false
         column_index = 0
-
         row_index += 1
     end
 
     sum_of_number
 end
 
+def solve_part_two(matrix)
+    row_index = 0
+    column_index = 0
+    number = ''
+    gear_number_hash = {}
+    coordinate_array = Set.new
+
+    while row_index <= matrix.length - 1
+        while column_index <= matrix[row_index].length - 1
+            if is_number(matrix[row_index][column_index])
+                number += matrix[row_index][column_index]
+                number_start_index = column_index if number_start_index.nil?
+                safe_has_adjacent_gear(coordinate_array, matrix, row_index, column_index-1) || # left
+                safe_has_adjacent_gear(coordinate_array, matrix, row_index, column_index+1) || # right
+                safe_has_adjacent_gear(coordinate_array, matrix, row_index+1, column_index) || # up
+                safe_has_adjacent_gear(coordinate_array, matrix, row_index-1, column_index) || # down
+                safe_has_adjacent_gear(coordinate_array, matrix, row_index+1, column_index+1) || # up and right
+                safe_has_adjacent_gear(coordinate_array, matrix, row_index+1, column_index-1) || # up and left
+                safe_has_adjacent_gear(coordinate_array, matrix, row_index-1, column_index+1) || # down and right
+                safe_has_adjacent_gear(coordinate_array, matrix, row_index-1, column_index-1) # down and left
+            else
+                coordinate_array.each do |coordinate|
+                    if gear_number_hash.has_key?(coordinate)
+                        gear_number_hash[coordinate] << Integer(number)
+                    else
+                        gear_number_hash[coordinate] = [Integer(number)]
+                    end
+                end
+                number = ''
+                coordinate_array = Set.new
+            end
+            column_index += 1
+        end
+
+        coordinate_array.each do |coordinate|
+            if gear_number_hash.has_key?(coordinate)
+                gear_number_hash[coordinate] << Integer(number)
+            else
+                gear_number_hash[coordinate] = [Integer(number)]
+            end
+        end
+        number = ''
+
+        coordinate_array = Set.new
+        row_index += 1
+        column_index = 0
+    end
+    gear_number_hash.delete_if { |key, value| value.size != 2 }
+    gear_number_hash.map{|key, value| value.reject(&:zero?).inject(:*)}.inject(:+)
+end
+
 def safe_is_symbol(matrix, row, col)
     return false unless row.between?(0, matrix.length - 1) && col.between?(0, matrix[row].length - 1)
     is_symbol(matrix[row][col])
+end
+
+def safe_has_adjacent_gear(coordinate_array, matrix, row, col)
+    return false unless row.between?(0, matrix.length - 1) && col.between?(0, matrix[row].length - 1)
+    if matrix[row][col] == '*'
+        coordinate_array.add([row, col])
+    end
 end
 
 def is_symbol(char)
@@ -66,3 +134,7 @@ input = File.readlines("day3/input.txt").map(&:chomp)
 # Part 1
 puts "Example Part 1: #{solve_part_one(p1_example)}"
 puts "Part 1: #{solve_part_one(input)}"
+
+# Part 2
+puts "Example Part 2: #{solve_part_two(p1_example)}"
+puts "Part 2: #{solve_part_two(input)}"
---
+++ day4/solution.rb
@@ -0,0 +1,79 @@
+def solve_part_one(games)
+
+    sum_points = 0
+
+    games.each do |game|
+        game_split = game.split(':')
+        numbers = game_split[1].split('|')
+        my_numbers = numbers[0].split
+        winning_numbers = numbers[1].split
+        points = 0
+
+        correct_numbers = my_numbers.length - (my_numbers - winning_numbers).length
+
+        if correct_numbers == 1
+            points += 1
+        elsif correct_numbers > 1
+            points += 2**(correct_numbers - 1)
+        end
+
+        sum_points += points
+    end
+    
+    sum_points
+end
+
+def solve_part_two(games)
+
+    game_hash = { }
+    sum_total_games = 0
+
+    games.each do |game|
+        game_split = game.split(':')
+        game_id = Integer(game_split[0].split[1])
+        numbers = game_split[1].split('|')
+        my_numbers = numbers[0].split
+        winning_numbers = numbers[1].split
+        multiplier = 1
+
+        if game_hash.has_key?(game_id)
+            game_hash[game_id] += 1
+        else
+            game_hash[game_id] = 1
+        end
+
+        if game_hash.has_key?(game_id)
+            multiplier = game_hash[game_id]
+            sum_total_games += game_hash[game_id]
+        else
+            sum_total_games += 1
+        end
+
+        correct_numbers = my_numbers.length - (my_numbers - winning_numbers).length
+
+        (1..correct_numbers).each do |i|
+            won_id = game_id + i
+            if game_hash.has_key?(won_id)
+                game_hash[won_id] += multiplier
+            else
+                game_hash[won_id] = multiplier
+            end
+        end
+
+    end
+
+    sum_total_games
+end
+
+# Files
+p1_example = File.readlines("day4/p1_example.txt")
+p2_example = File.readlines("day4/p2_example.txt")
+input = File.readlines("day4/input.txt").map(&:chomp)
+
+# Part 1
+puts "Example Part 1: #{solve_part_one(p1_example)}"
+puts "Part 1: #{solve_part_one(input)}"
+
+# Part 2
+puts "Example Part 2: #{solve_part_two(p1_example)}"
+puts "Part 2: #{solve_part_two(input)}"
